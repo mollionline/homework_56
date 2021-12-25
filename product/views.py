85e2a98dd8975738e53c3054876c5a947422798f
moldir <@auezova_moldir>
2021-12-26 00:44:53 +0600

    #2 Добавила вью для отображения продуктов фактическое время 4 часа

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from product.models import Product, Category
+
+def product_view(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request=request,
+                  template_name='product_list.html',
+                  context=context)

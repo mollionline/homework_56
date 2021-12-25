eccad3f33fa6df1c76ff9822d514df9607673e8f
moldir <@auezova_moldir>
2021-12-26 00:41:21 +0600

    #2 Добавила отображение в админку и добавила файл

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from product.models import Category, Product
+
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'description']
+    list_filter = ['category']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'description', 'created_at', 'price', 'image']
+    list_filter = ['product']
+    search_fields = ['product', 'created_at']
+    fields = ['product', 'category', 'description', 'created_at', 'price', 'image']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)

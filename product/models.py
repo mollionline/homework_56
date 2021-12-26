ecf815a126e98315a9ba4a8a09e15c617d731e69
moldir <@auezova_moldir>
2021-12-26 15:51:32 +0600

    #5 Добавила в категорию уникальность данных в базе, чтобы при добавлении не повторялись категории

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category = models.TextField(max_length=100, null=False, blank=False)
+    category = models.TextField(max_length=100, null=False, blank=False, unique=True)
     description = models.TextField(max_length=500, null=True, blank=True)
 
     def __str__(self):

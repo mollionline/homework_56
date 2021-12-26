1aab5fed8663b3cf812d969f6a249b7824e39be3
moldir <@auezova_moldir>
2021-12-26 13:06:15 +0600

    #3 Добавила вью для детального просмотра продукта, фактическое время 1 час

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from product.views import products_view
+from product.views import products_view, product_view
 
 urlpatterns = [
-    path('', products_view, name='product_list')
+    path('', products_view, name='product_list'),
+    path('products/?<int:pk>/', product_view, name='product_detail')
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from product.models import Product, Category
+
 
 def products_view(request):
     products = Product.objects.all()
@@ -10,4 +11,14 @@ def products_view(request):
     }
     return render(request=request,
                   template_name='product_list.html',
+                  context=context)
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request=request,
+                  template_name='product_detail.html',
                   context=context)

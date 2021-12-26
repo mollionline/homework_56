fefc96e5131b7e9a3012cd36b4d23f65243ace75
moldir <@auezova_moldir>
2021-12-26 21:13:52 +0600

    #6 Добавила вью для добавления продукта, фактическое время выполнения 3 часа

--- product/views.py
+++ product/views.py
@@ -40,3 +40,29 @@ def category_add_view(request):
             return HttpResponseRedirect('/')
         except BaseException:
             return HttpResponseRedirect("<h2>Категория существует/h2>")
+
+
+def product_add_view(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    if request.method == 'GET':
+        return render(request=request, template_name='product_add_page.html', context=context)
+    elif request.method == 'POST':
+        product = request.POST.get('product')
+        description = request.POST.get('description')
+        category = request.POST.get('category')
+        created_at = request.POST.get('created_at')
+        price = request.POST.get('price')
+        image = request.POST.get('image')
+        added_product = Product.objects.create(
+            product=product,
+            description=description,
+            category=Category.objects.get(pk=category),
+            created_at=created_at,
+            price=price,
+            image=image
+        )
+        url = reverse('product_detail', kwargs={'pk': added_product.pk})
+        return HttpResponseRedirect(url)

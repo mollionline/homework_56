287000edb2a26c0a83ea4d04205074dc5cae98a3
moldir <@auezova_moldir>
2021-12-26 21:13:02 +0600

    #6 Добавила путь для добавления продукта, фактическое время выполнения 3 часа

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from product.views import products_view, product_view, category_add_view
+from product.views import products_view, product_view, category_add_view, product_add_view
 
 urlpatterns = [
     path('', products_view, name='product_list'),
     path('products/?<int:pk>/', product_view, name='product_detail'),
-    path('categories/add/', category_add_view, name='category_add')
+    path('categories/add/', category_add_view, name='category_add'),
+    path('products/add/', product_add_view, name='product_add')
 ]

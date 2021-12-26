46d6a27d1bbd34d80bbd2a67cf19443bbe8d7ff2
moldir <@auezova_moldir>
2021-12-27 00:10:58 +0600

    #7 Убрала лишние нули с цены товара, фактическое время выполнения 3 часа

--- product/models.py
+++ product/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField(max_length=200, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    price = models.DecimalField(max_digits=19, decimal_places=3, null=False, blank=False)
+    price = models.DecimalField(max_digits=19, decimal_places=0, null=False, blank=False)
     image = models.TextField(max_length=500, null=False, blank=False)
 
     def __str__(self):

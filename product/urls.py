8867eb31a4d339b063ac1e505d46881c2782b5d2
moldir <@auezova_moldir>
2021-12-26 15:52:44 +0600

    #5 Добавила в путь для добавления категории

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from product.views import products_view, product_view
+from product.views import products_view, product_view, category_add_view
 
 urlpatterns = [
     path('', products_view, name='product_list'),
-    path('products/?<int:pk>/', product_view, name='product_detail')
+    path('products/?<int:pk>/', product_view, name='product_detail'),
+    path('categories/add/', category_add_view, name='category_add')
 ]

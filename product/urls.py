b775cc8640187a3c38f588f961999a3e19c64baf
moldir <@auezova_moldir>
2021-12-26 12:24:06 +0600

    #4 Переименовала вью product_view, фактическое время 10 минут

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from product.views import product_view
+from product.views import products_view
 
 urlpatterns = [
-    path('', product_view, name='product_list')
+    path('', products_view, name='product_list')
 ]

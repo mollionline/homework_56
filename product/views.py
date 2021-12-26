2aae15e3d5b3a44f982cddee91d91bf946af9194
moldir <@auezova_moldir>
2021-12-26 15:53:57 +0600

    #5 Добавила вью для добавления категории, фактическое время 2 часа

--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 # Create your views here.
 from product.models import Product, Category
@@ -22,3 +24,19 @@ def product_view(request, pk):
     return render(request=request,
                   template_name='product_detail.html',
                   context=context)
+
+
+def category_add_view(request):
+    if request.method == 'GET':
+        return render(request, 'category_add_page.html')
+    elif request.method == 'POST':
+        try:
+            category = request.POST.get('category')
+            description = request.POST.get('description')
+            added_cat = Category.objects.create(
+                category=category,
+                description=description
+            )
+            return HttpResponseRedirect('/')
+        except BaseException:
+            return HttpResponseRedirect("<h2>Категория существует/h2>")

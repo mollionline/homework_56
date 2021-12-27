b6db9b1103af12413dff259c5e7d21f7047ed39b
moldir <@auezova_moldir>
2021-12-27 21:42:42 +0600

    #9 Добавила маршруты удаления и редактирования, фактическое время выполнения 4 часа

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from product.views import products_view, product_view, category_add_view, product_add_view, categories_view, \
-    category_view, category_edit_view, category_delete_view
+    category_view, category_edit_view, category_delete_view, product_edit_view, product_delete_view
 
 urlpatterns = [
     path('', products_view, name='product_list'),
@@ -10,5 +10,7 @@ urlpatterns = [
     path('categories/', categories_view, name='categories_list'),
     path('categories/?<int:pk>/', category_view, name='category_detail'),
     path('categories/?<int:pk>/edit', category_edit_view, name='category_edit'),
-    path('categories/?<int:pk>/delete', category_delete_view, name='category_delete')
+    path('categories/?<int:pk>/delete', category_delete_view, name='category_delete'),
+    path('products/?<int:pk>/edit', product_edit_view, name='product_edit'),
+    path('products/?<int:pk>/delete', product_delete_view, name='product_delete')
 ]

456e233fcd80da1ead141ca1204a623ecf23481d
moldir <@auezova_moldir>
2021-12-27 14:55:38 +0600

    #8 Добавила вью, фактическое время выполнения 4 часа

--- product/views.py
+++ product/views.py
@@ -37,9 +37,52 @@ def category_add_view(request):
                 category=category,
                 description=description
             )
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/categories/')
         except BaseException:
             return HttpResponseRedirect("<h2>Категория существует/h2>")
+
+
+def categories_view(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request=request,
+                  template_name='categories_list.html',
+                  context=context)
+
+
+def category_view(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    context = {
+        'category': category
+    }
+    return render(request=request,
+                  template_name='category_detail.html',
+                  context=context)
+
+
+def category_edit_view(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+        if request.method == 'POST':
+            category.category = request.POST.get('category')
+            category.description = request.POST.get('description')
+            category.save()
+            return HttpResponseRedirect('/categories/')
+        else:
+            return render(request, 'category_edit.html', {'category': category})
+    except Category.DoesNotExist:
+        return HttpResponseNotFound("<h2>Category not found</h2>")
+
+
+def category_delete_view(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return HttpResponseRedirect('/categories/')
+    except Category.DoesNotExist:
+        return HttpResponseNotFound("<h2>Category not found</h2>")
 
 
 def product_add_view(request):
@@ -53,14 +96,12 @@ def product_add_view(request):
         product = request.POST.get('product')
         description = request.POST.get('description')
         category = request.POST.get('category')
-        created_at = request.POST.get('created_at')
         price = request.POST.get('price')
         image = request.POST.get('image')
         added_product = Product.objects.create(
             product=product,
             description=description,
             category=Category.objects.get(pk=category),
-            created_at=created_at,
             price=price,
             image=image
         )

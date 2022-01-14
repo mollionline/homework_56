fdfa378de062968a0fb431d6eac2556f49feb027
moldir <@auezova_moldir>
2022-01-14 22:42:51 +0600

    #2 В модели others категории переименовала на Разное, фактическое время выполнения 1 час

--- product/models.py
+++ product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category = models.TextField(max_length=100, null=False, blank=False, unique=True)
+    category = models.TextField(max_length=100, null=False, blank=False, unique=True, default='Разное')
     description = models.TextField(max_length=500, null=True, blank=True)
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     product = models.TextField(max_length=100, null=False, blank=False)
-    description = models.TextField(max_length=2000, null=True, blank=True, default='Other')
+    description = models.TextField(max_length=2000, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)

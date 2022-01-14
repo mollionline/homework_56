2e0cf7e80c0ce1ca1bb703de62bc2705da161a56
moldir <@auezova_moldir>
2022-01-14 20:15:44 +0600

    #1 Написала модель для протатипа магазина, фактическое время выполнения 1 час

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -12,10 +13,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     product = models.TextField(max_length=100, null=False, blank=False)
-    description = models.TextField(max_length=200, null=True, blank=True)
+    description = models.TextField(max_length=2000, null=True, blank=True, default='Other')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    price = models.DecimalField(max_digits=19, decimal_places=0, null=False, blank=False)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
+    remains = models.IntegerField(validators=[MinValueValidator(0)], default=0)
     image = models.TextField(max_length=500, null=False, blank=False)
 
     def __str__(self):
